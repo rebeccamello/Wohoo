d34d2a2d2682359738732f6a86e0f542e2cfcac9
Rebecca Mello <rebecca.mello@me.com>
2021-06-25 18:12:50 -0300

    Att do storyboard e arquivos
    - Linkagem da página de Respostas com a de Help
    - Adição do arquivo do EmAltaViewController
    - Atualização das imagens de fundo dos cards
    - Deleção do arquivo de FavoritosViewController

--- FirstApp/ViewControllers/EmAltaViewController.swift
+++ FirstApp/ViewControllers/EmAltaViewController.swift
@@ -6,9 +6,9 @@
 ////
 //
 import UIKit
-class CollectionViewController: UIViewController {
-    let images = ["capaLogo", "capaMarcas", "capaServico"]
-
+class EmAltaViewController: UIViewController {
+    let images = ["capaAlta"]
+    
     var perguntas:[Pergunta] = Pergunta.logos
     
     var selectedIndex: Int = 0
@@ -19,29 +19,9 @@ class CollectionViewController: UIViewController {
         super.viewDidLoad()
         
     }
-    @IBOutlet weak var segmentedOutlet: UISegmentedControl!
-    
-    
-    @IBAction func segmentedControl(_ sender: Any) {
-        
-        let index = self.segmentedOutlet.selectedSegmentIndex
-        if index == 0 {
-            self.perguntas = Pergunta.logos
-        }
-        else if index == 1{
-            self.perguntas = Pergunta.marcas
-        }
-        else{
-            self.perguntas = Pergunta.servicos
-        }
-        
-        collectionView.reloadData()
-        collectionView.reloadItems(at: collectionView.indexPathsForVisibleItems)
-    }
-    
 }
 
-class PostCell: UICollectionViewCell{
+class EmAltaPostCell: UICollectionViewCell{
     @IBOutlet weak var background: UIView!
     @IBOutlet weak var image: UIImageView!
     @IBOutlet weak var question: UILabel!
@@ -52,22 +32,22 @@ class PostCell: UICollectionViewCell{
     }
     
 }
-extension CollectionViewController: UICollectionViewDataSource{
+extension EmAltaViewController: UICollectionViewDataSource{
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         
         return perguntas.count
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "postCell", for: indexPath) as! PostCell
-        cell.image.image = UIImage(named: images[self.segmentedOutlet.selectedSegmentIndex])
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "postCell", for: indexPath) as! EmAltaPostCell
+        cell.image.image = UIImage()
         cell.question.text = perguntas[indexPath.row].nomePergunta
         return cell
     }
     
 }
 
-extension CollectionViewController: UICollectionViewDelegate{
+extension EmAltaViewController: UICollectionViewDelegate{
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
         selectedIndex = indexPath.row
     }
